2861e2ac2351569845cd36a2672d1df2f01551f7
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-02-18 03:35:18 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+  
+  attr_accessor :word
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(words_array)
+    words_array.select do |word|
+      word.delete(' ').chars.sort == @word.chars.sort
+    end
+  end
+  
+end
